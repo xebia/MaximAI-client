474d6f09878d4900f1766bd9321f948d80d09425
Julian de Ruiter <julianderuiter@gmail.com>
2024-04-25 12:11:36 +0200

    Format.

--- src/maximai_client/main.py
+++ src/maximai_client/main.py
@@ -23,46 +23,47 @@ from pydub import AudioSegment
 from pydub.playback import play
 import typer
 
-END_PHRASES = {
-    "by max.",
-    "bye max.",
-    "goodbye max."
-}
+END_PHRASES = {"by max.", "bye max.", "goodbye max."}
 
 
-def main(*,
-        access_key: Annotated[str, typer.Option(envvar="PV_ACCESS_KEY")],
-        wakeword_model_path: Path = "models/wakeword.ppn",
-        endpoint_duration_sec: float = 1.0,
-        disable_automatic_punctuation: bool = False,
-        show_audio_devices: bool = False,
-        audio_device_index: int = -1
-    ):
+def main(
+    *,
+    access_key: Annotated[str, typer.Option(envvar="PV_ACCESS_KEY")],
+    wakeword_model_path: Path = "models/wakeword.ppn",
+    endpoint_duration_sec: float = 1.0,
+    disable_automatic_punctuation: bool = False,
+    show_audio_devices: bool = False,
+    audio_device_index: int = -1,
+):
     if show_audio_devices:
         for index, name in enumerate(PvRecorder.get_available_devices()):
-            print('Device #%d: %s' % (index, name))
+            print("Device #%d: %s" % (index, name))
         return
 
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
 
-    porcupine = pvporcupine.create(access_key=access_key, keyword_paths=[wakeword_model_path])
+    porcupine = pvporcupine.create(
+        access_key=access_key, keyword_paths=[wakeword_model_path]
+    )
 
     cheetah = pvcheetah.create(
         access_key=access_key,
         # library_path=args.library_path,
         # model_path=args.model_path,
         endpoint_duration_sec=endpoint_duration_sec,
-        enable_automatic_punctuation=not disable_automatic_punctuation
+        enable_automatic_punctuation=not disable_automatic_punctuation,
     )
 
     orca = pvorca.create(access_key=access_key)
     orca
 
     try:
-        recorder = PvRecorder(frame_length=cheetah.frame_length, device_index=audio_device_index)
+        recorder = PvRecorder(
+            frame_length=cheetah.frame_length, device_index=audio_device_index
+        )
 
-        logging.info('Listening... (press Ctrl+C to stop)')
+        logging.info("Listening... (press Ctrl+C to stop)")
 
         # listen_conversation(recorder, cheetah)
 
@@ -85,8 +86,11 @@ def main(*,
             recorder.stop()
     except KeyboardInterrupt:
         pass
-    except (pvcheetah.CheetahActivationLimitError, pvporcupine.PorcupineActivationLimitError):
-        logging.error('AccessKey has reached its processing limit.')
+    except (
+        pvcheetah.CheetahActivationLimitError,
+        pvporcupine.PorcupineActivationLimitError,
+    ):
+        logging.error("AccessKey has reached its processing limit.")
     finally:
         porcupine.delete()
         cheetah.delete()
@@ -112,7 +116,7 @@ def listen_query(recorder, cheetah, logger) -> str:
     while not is_endpoint:
         partial_transcript, is_endpoint = cheetah.process(recorder.read())
         transcript += partial_transcript
-        print(partial_transcript, end='', flush=True)
+        print(partial_transcript, end="", flush=True)
     transcript += cheetah.flush()
 
     recorder.stop()
@@ -126,7 +130,7 @@ def generate_response(query: str) -> str:
 
 
 def say_response(orca, response, file_name="speech.wav"):
-    logging.info('Generating response')
+    logging.info("Generating response")
     with tempfile.TemporaryDirectory() as tmp_dir:
         output_path = Path(tmp_dir) / file_name
         orca.synthesize_to_file(response, output_path=str(output_path), speech_rate=1.0)
@@ -135,6 +139,6 @@ def say_response(orca, response, file_name="speech.wav"):
         play(song)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     dotenv.load_dotenv()
     typer.run(main)
